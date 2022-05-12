65f20de09d4e57907f21306bb49541a8059473d0
Roberta Bernardo <99690987+rlbernardo@users.noreply.github.com>
2022-05-12 16:33:28 -0300

    importando módulo e testando a janela com tkinter

---
+++ madlib1.py
@@ -0,0 +1,9 @@
+# importando o módulo
+from tkinter import *
+
+# Inicialização da Janela do mid lab no windows
+root = Tk()
+root.geometry('500x500')
+root.title('DataFlair-Mad Libs Generator')
+Label(root, text='Gerador de MAD LIBS \n Divirta-se!', font='arial 25 bold').pack()
+Label(root, text='Escolha um e clique :', font='arial 20 bold').place(x=40, y=80)
