c7995f610bd69ba66264847d538561d6813b5762
Roberta Bernardo <99690987+rlbernardo@users.noreply.github.com>
2022-05-12 16:46:38 -0300

    Add files via upload
    adicionando funções e concatenando strings com bases nas fantasias

--- madlib1.py
+++ madlib1.py
@@ -7,3 +7,12 @@ root.geometry('500x500')
 root.title('DataFlair-Mad Libs Generator')
 Label(root, text='Gerador de MAD LIBS \n Divirta-se!', font='arial 25 bold').pack()
 Label(root, text='Escolha um e clique :', font='arial 20 bold').place(x=40, y=80)
+
+################Fantasias##############
+
+def madlib1_aventura():
+    nome = input('coloque seu nome : ')
+    print(
+        # Meu nome é + nome + ', sou +
+
+   root.mainloop()
